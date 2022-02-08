a7ce876cb6e4d5af2b48018a93afe5b6c71bace1
fukurose <xylitoall@gmail.com>
2022-02-08 21:44:46 +0900

    handshake from the server

--- main.go
+++ main.go
@@ -1,19 +1,45 @@
 package main
 
 import (
+	"crypto/sha1"
+	"encoding/base64"
 	"fmt"
 	"log"
 	"net/http"
-	"net/http/httputil"
 )
 
 func handlerWebSocket(w http.ResponseWriter, r *http.Request) {
-	dump, err := httputil.DumpRequest(r, true)
-	if err != nil {
-		http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+	if r.Header.Get("Connection") != "Upgrade" || r.Header.Get("Upgrade") != "websocket" {
+		fmt.Println("error")
+		w.WriteHeader(400)
 		return
 	}
-	fmt.Println(string(dump))
+
+	// ここからのやりとりは、HTTP protocol ではなくなるので、 Hijacker を使う
+	hijacker := w.(http.Hijacker)
+	conn, readWriter, err := hijacker.Hijack()
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	// ブラウザからのキーを元にして、レスポンス用のキーを作成
+	key := r.Header.Get("Sec-Websocket-Key")
+	acceptKey := buildAcceptKey(key)
+
+	readWriter.WriteString("HTTP/1.1 101 Switching Protocols\r\n")
+	readWriter.WriteString("Upgrade: websocket\r\n")
+	readWriter.WriteString("Connection: Upgrade\r\n")
+	readWriter.WriteString("Sec-WebSocket-Accept: " + acceptKey + "\r\n")
+	readWriter.WriteString("\r\n") // 空白行でステータスラインの終わりを示す
+	readWriter.Flush()
+}
+
+func buildAcceptKey(key string) string {
+	h := sha1.New()
+	h.Write([]byte(key))
+	h.Write([]byte("258EAFA5-E914-47DA-95CA-C5AB0DC85B11"))
+	return base64.StdEncoding.EncodeToString(h.Sum(nil))
 }
 
 func main() {
