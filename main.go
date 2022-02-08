ba45436db2e8b8e545be81c2270da312637d61c7
fukurose <xylitoall@gmail.com>
2022-02-08 21:17:14 +0900

    handshake from the client

--- main.go
+++ main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"net/http/httputil"
 )
+
+func handlerWebSocket(w http.ResponseWriter, r *http.Request) {
+	dump, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+		return
+	}
+	fmt.Println(string(dump))
+}
 
 func main() {
 	var httpServer http.Server
 	http.Handle("/", http.FileServer(http.Dir("public")))
+	http.HandleFunc("/websocket", handlerWebSocket)
 	log.Println("start http listening :3000")
 	httpServer.Addr = ":3000"
 	log.Println(httpServer.ListenAndServe())
